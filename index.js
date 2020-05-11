622e98504c396fd39837c753f83fa2c8e417b7b6
ineainea <58885393+ineainea@users.noreply.github.com>
2020-05-11 18:31:25 +0900

    .

---
+++ index.js
@@ -0,0 +1,12 @@
+const nicoInfo = {
+    name:"Nicolas",
+    age:33,
+    gender:"Male",
+    isHansome:true
+}
+
+console.log(nicoInfo.gender);
+
+nicoInfo.gender = "Female"
+
+console.log(nicoInfo.gender);

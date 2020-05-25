bf0321baf14ca58cf3a4515536840411acad21bf
ineainea <58885393+ineainea@users.noreply.github.com>
2020-05-25 18:15:30 +0900

    .
    .

--- DOM_If_Else_Function2.js
+++ DOM_If_Else_Function2.js
@@ -1,14 +1,9 @@
 const title = document.querySelector("#title"); /* HTML을 이 DOM 객체로 바꿈 */
 
-const CLICKED_CLASS = "cliecked";
+const CLICKED_CLASS = "clicked"; // CSS에 class clicked를 변수 const CLICKED_CLASS에 저장.
 
 function handleClick(){
-    const currentClass = title.className;
-    if(currentClass !== CLICKED_CLASS){
-        title.className = CLICKED_CLASS
-    } else{
-        title.className = "";
-    }
+    title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
---
+++ clock.js
@@ -0,0 +1,23 @@
+const clockContainer = document.querySelector(".js-clock"), //html div.class "js-clock"를 불러옴
+    clockTitle = clockContainer.querySelector(".js-title"); // h1 class 를 가져옴
+
+function getTime()
+{
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    const milliseconds = date.getMilliseconds();
+    clockTitle.innerText = `${
+        hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes}:${
+        seconds < 10 ? `0${seconds}` : seconds}:${milliseconds}`
+}
+
+function init() // clock 기능 최종 실행 함수
+{
+    getTime();
+    setInterval(getTime, 1);
+}
+
+init();
